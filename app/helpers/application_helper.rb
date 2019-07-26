5fc0f6bc10836e28d08578ff4e66e4ae0dbff3d2
gablooblue <gabrielhenry.lopez@gmail.com>
2019-07-26 14:03:41 +0800

    Add default meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def title(text)
     content_for :title, text
   end
+
+  def default_meta_tags
+    {
+      twitter: {image: "s3://elasticbeanstalk-ap-southeast-1-740434333056/spillstreet.PNG", card: "summary"},
+      og: {image: "s3://elasticbeanstalk-ap-southeast-1-740434333056/spillstreet.PNG"}
+    }
+  end
 end
